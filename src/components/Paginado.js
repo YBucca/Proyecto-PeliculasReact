56fc3eecae0a85c400b723ccbf6143e3a5f10bdf
YBucca <yani.bucca@gmail.com>
2022-04-04 20:54:25 -0300

    agrego paginado modo oscuro

--- src/components/Paginado.js
+++ src/components/Paginado.js
@@ -1,14 +1,19 @@
 import Pagination from "@mui/material/Pagination";
+import Box from "@mui/material/Box";
+import { useContext } from "react";
+import Context from "../context/Context";
 const Paginado = ({ handleChange, page, totalPages }) => {
+	const context = useContext(Context);
 	return (
-		<div>
+		<Box sx={{ bgcolor: context.modoClaro ? "transparent" : "#9f86c0", width:"400px",p:2, borderRadius:10, mt:4}}>
 			<Pagination
 				count={totalPages}
 				color="secondary"
 				page={page}
 				onChange={handleChange}
+				sx={{ color: "white" }}
 			/>
-		</div>
+		</Box>
 	);
 };
 
