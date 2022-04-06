07c7b6860d6075b91231553734c6abca238598c7
YBucca <yani.bucca@gmail.com>
2022-04-05 21:33:20 -0300

    agrego input y boton de material ui

--- src/components/Buscar.js
+++ src/components/Buscar.js
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Cards from "./Cards";
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import {
+	FormControl,
+	InputLabel,
+	Input,
+	Button,
+} from "@mui/material";
+
+
 
 const Buscar = () => {
 	const [valorDelInput, setValorDelInput] = useState(""); // para hacer click en el boton enviar
@@ -17,6 +26,8 @@ const Buscar = () => {
 			.then((data) => {
 				if (data.results) {
 					setPeliculas(data.results);
+				}else {
+
 				}
 				// para que al buscar algo que no sea de la api , no se actualice y se borre todo. hacer una imagen de error
 			});
@@ -31,30 +42,56 @@ const Buscar = () => {
 		});
 	};
 	return (
-		<div className="buscador">
-			Buscar peliculas
+		<>
+			<Grid container sx={{ p: 3 }}>
+				<Grid item md={12}>
+					<FormControl sx={{ width: "50%" }}>
+						<InputLabel htmlFor="my-input">
+							Ingresá una película
+						</InputLabel>
+						<Input
+							id="buscador"
+							aria-describedby="buscador-peliculas"
+							onChange={handleChange}
+							value={valorDelInput}
+						/>
+					</FormControl>
+					<Button
+						onClick={handleClick}
+						variant="contained"
+						color="secondary"
+						size="small"
+						sx={{ m: 2 }}
+					>
+						Buscar
+					</Button>
+				</Grid>
+			</Grid>
+			<div className="buscador">
+				{/* Buscar peliculas
 			<input
 				type="text"
 				onChange={handleChange}
 				value={valorDelInput}
-			></input>
-			<button onClick={handleClick}>Buscar</button>
-			{peliculas.map((pelicula) => (
-				<Link
-					style={{
-						textDecoration: "none",
-						color: "#9f86c0",
-					}}
-					to={`/movie/${pelicula.id}`}
-					key={pelicula.id}
-				>
-					<Cards
-						titulo={pelicula.title}
-						imagen={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
-					/>
-				</Link>
-			))}
-		</div>
+			></input> */}
+				{/* <button onClick={handleClick}>Buscar</button> */}
+				{peliculas.map((pelicula) => (
+					<Link
+						style={{
+							textDecoration: "none",
+							color: "#9f86c0",
+						}}
+						to={`/movie/${pelicula.id}`}
+						key={pelicula.id}
+					>
+						<Cards
+							titulo={pelicula.title}
+							imagen={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
+						/>
+					</Link>
+				))}
+			</div>
+		</>
 	);
 };
 
