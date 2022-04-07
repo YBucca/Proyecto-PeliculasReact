1ee80df997f45a314a94ae0072caaa02b3081b78
YBucca <yani.bucca@gmail.com>
2022-04-06 21:31:21 -0300

    agrego estilo

--- src/components/Paginado.js
+++ src/components/Paginado.js
@@ -5,7 +5,7 @@ import Context from "../context/Context";
 const Paginado = ({ handleChange, page, totalPages }) => {
 	const context = useContext(Context);
 	return (
-		<Box sx={{ bgcolor: context.modoClaro ? "transparent" : "#9f86c0", width:"400px",p:2, borderRadius:10, mt:4}}>
+		<Box sx={{ bgcolor: context.modoClaro ? "transparent" : "#9f86c0", width:"500px",p:2, borderRadius:10, mt:4}}>
 			<Pagination
 				count={totalPages}
 				color="secondary"
