c6cc2d62d04df9ab062346693cf5b9c58e2af1e0
YBucca <yani.bucca@gmail.com>
2022-04-16 20:41:14 -0300

    cambio de tamaño para mobile

--- src/components/Paginado.js
+++ src/components/Paginado.js
@@ -5,7 +5,8 @@ import Context from "../context/Context";
 const Paginado = ({ handleChange, page, totalPages }) => {
 	const context = useContext(Context);
 	return (
-		<Box sx={{ bgcolor: context.modoClaro ? "transparent" : "#9f86c0", width:"500px",p:2, borderRadius:10, mt:4}}>
+		<Box sx={{ bgcolor: context.modoClaro ? "transparent" : "#9f86c0", width:"500px",p:2, borderRadius:10, mt:4}} className="pagination">
+		
 			<Pagination
 				count={totalPages}
 				color="secondary"
