b6af60631cb8692c65ed5e5b92f7b7819cff9533
YBucca <yani.bucca@gmail.com>
2022-04-20 06:43:09 -0300

    fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,9 @@
-import { Link } from "react-router-dom";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
 import { useContext, useState } from "react";
 import Context from "../context/Context";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import NightlightIcon from "@mui/icons-material/Nightlight";
-import IconButton from "@mui/material/IconButton";
-import Selectlang from "./Selectlang";
-import { cambioLenguaje } from "../utils/variables";
 import Hamburger from "hamburger-react";
-import SearchIcon from "@mui/icons-material/Search";
 import NavList from "./NavList";
 
 const NavBar = () => {
